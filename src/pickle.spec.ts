42847185d0ae455a8f61780ec802f2d817c5e95a
joaopfg <joao.paulo@eyepick.co>
2022-06-13 14:57:29 +0200

    [ADD] unit tests

---
+++ src/pickle.spec.ts
@@ -0,0 +1,12 @@
+import { pickle } from './pickle';
+
+import * as chai from 'chai';
+
+const expect = chai.expect;
+
+describe('Testing loading of a string', () => {
+    it('loads after dumps of a string should be equivalent to identity' , () => {
+        expect(pickle.loads(pickle.dumps("test"))).to.equal("test");
+    });
+});
+
